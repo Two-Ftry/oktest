004ae4efe43761e5f3c92373191e0eb0e3f00e94
jfhuang <jfhuang@jfhuangdeMacBook-Air.local>
2016-08-30 00:13:36 +0800

    ddd

--- webpack/websrc/webpack.config.js
+++ webpack/websrc/webpack.config.js
@@ -2,7 +2,16 @@
 const webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var path = require('path');
+var fs = require('fs');
+var Q = require('q');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+readEntryFiles(path.resolve(__dirname, 'business'), '.entry.js').then(function(data){
+  console.log('oooooooo-----xxxx', data);
+}, function(error){
+  console.log('entryFiles', error);
+});
+
 
 module.exports = {
   context: path.resolve(__dirname),
@@ -51,3 +60,70 @@ module.exports = {
     })
   ]
 };
+
+/* 读取entry.js后缀的文件 */
+function readEntryFiles(filePath, filter){
+  var deferred = Q.defer();
+  fs.readdir(filePath, function(err, files){
+    var result = {};
+    files.forEach(function(filename, index){
+      console.log('index ', index, files.length);
+      var p = path.resolve(filePath, filename);
+      getFileStat(p, filename, filter).then(function(data){
+        contactObjs(result, data);
+        console.log('readEntryFiles', result);
+        if(index == files.length-1){
+          deferred.resolve(result);
+        }
+      }, function(){
+        deferred.reject({msg: 'error!!!'});
+      });
+    });
+
+  });
+  return deferred.promise;
+}
+
+function getFileStat(p, filename, filter){
+  var deferred = Q.defer();
+  fs.stat(p, function(err, stats){
+    if(stats.isFile()){
+      var result = {};
+      addFile(result, filename, filter);
+      deferred.resolve(result);
+    }else if(stats.isDirectory()){
+      readEntryFiles(p, filter).then(function(data){
+        // contactObjs(result, data);
+        console.log('getFileStat', data);
+        deferred.resolve(data);
+      }, function(){
+        deferred.reject({msg:'nonono!!!'});
+        console.log('readEntryFiles fail!!!');
+      });
+    }
+  });
+  return deferred.promise;
+}
+
+function contactObjs(obj1, obj2){
+  for(var key in obj2){
+    obj1[key] = obj2[key];
+  }
+}
+
+function addFile(result, filename, filter){
+  var flag = false;
+  if(filter){
+    if(filename.indexOf(filter) != -1){
+      flag = true;
+    }
+  }else{
+    flag = true;
+  }
+  if(flag){
+    result[filename] = filename;
+    // fs.readFile(filename, function(data){
+    //   result[filename] = data;
+    // });
+  }
+}
